8c6023376e0cd3a128b61dbb923afd1ac8b4c461
Kelvin Marcano <kdog00131@gmail.comgit config --global user.name Kelvin>
2026-07-11 09:31:54 -0400

    Phase 2: Section 2 - create fleet list and loop through 5 robots

--- exercises/fleet_monitor.py
+++ exercises/fleet_monitor.py
@@ -23,7 +23,14 @@ class Robot:
         else:
             return False, self.front_sensor 
 
+robot_1 = Robot(2.5, 3.6, 1.0, "Scout") #Here I made a robot_1 object with 2.5 as the x coordinate value, 3.6 as the y coordinate value, 1.0 as the front sensor value and Scout as the name.
+robot_2 = Robot(-1.5,-2.1, 0.0, "Guard") #Here I made a robot_2 object with -1.5 as the x coordinate value, -2.1 as the y coordinate value, 0.0 as the front sensor value and Guard as the name.
+robot_3 = Robot(6.5, -1.9, 3.2, "Knight") #Here I made a robot_3 object with 6.5 as the x coordinate value, -1.9 as the y coordinate value, 3.2 as the front sensor value and Knight as the name.
+robot_4 = Robot(7.2, 0.0, 2.5, "Archer") #Here I made a robot_4 object with 7.2 as the x coordinate value, 0.0 as the y coordinate value, 2.5 as the front sensor value and Archer as the name.
+robot_5 = Robot(-1.1, -8.5, 5.0, "King") #Here I made a robot_5 object with -1.1 as the x coordinate value, -8.5 as the y coordinate value, 5.0 as the front sensor value and King as the name.
 
+fleet = [robot_1, robot_2, robot_3, robot_4, robot_5] #Here i made a fleet variable and inside it I have it store a list which holds the five robot objects.
 
-    
+for robot in fleet: #Here I made a for loop with a loop variable named robot and have it specifically use the fleet variable and the list its storing.
+    print(robot.name, robot.x, robot.y, robot.front_sensor) #Here I make a print statement that will print out each robots name, x coordinate value, y coordinate value, front_sensor value and name.
 
