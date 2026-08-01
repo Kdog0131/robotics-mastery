09ad30767794300035f68359e2fc92a451b77e37
Kelvin Marcano <kdog00131@gmail.comgit config --global user.name Kelvin>
2026-08-01 11:32:05 -0400

    Added get_3d_magnitude function, introduced 3D vector concepts

--- exercises/trig_movement.py
+++ exercises/trig_movement.py
@@ -49,6 +49,12 @@ def rotate_shape(points, theta_degrees): #Here I made a function named rotate_sh
         rotated_points.append((new_x, new_y)) #Here I have the rotated_points list add the numbers that the new_x and new_y variables are holding.
     return rotated_points #Here I made a return statement that returns the values that are in the list the rotated_points variable is storing.
 
+def get_3d_magnitude(dx,dy,dz): #Here I made a function named get_3d_magnitude with dx, dy and dz variables as its parameters/inputs
+    magnitude = math.sqrt(dx**2 + dy**2 + dz**2) #Here I made a magnitude variable and inside of it I have the math import use its square root function and inside of it I have the numbers that the dx,dy and dz variables are holding be to the second power, aswell as add them all together.
+    return magnitude #Here I made a return statement that returns the results of the magnitude variables calculations.
+
+
+
 result_magnitude, result_direction = get_distance_and_directions(3, 4) #Here I chain the result_magnitude and result direction variables and have them equal the get_distance_and_directions function with 3 and 4 in its parameters so that both variables are tied to the result of the function call.
 print("Magnitude:", round(result_magnitude,2)) #Here I made a print that prints Magnitude: and the magnitude result rounded 2 decimal places.
 print("Direction (degrees): ", round(result_direction, 2)) #Here I made a print that prints Direction (degrees): and the direction result rounded 2 decimal places.
@@ -59,3 +65,5 @@ print("Final position:", round(final_x, 2), round(final_y, 2)) #Here I made a pr
 square = [(1, 0), (0, 1), (-1, 0), (0, -1)] #Here I made a square variable and inside it I have it store a list of different tupples for the points on the square.
 rotated_square = rotate_shape(square, 90) #Here I made a rotated square variable and inside it I have it call the rotate_shape function with the square variable for the points using the tupples in the list the square variable is holding, and 90 for the theta_degrees in the functions parameters/inputs.
 print("Rotated square:", rotated_square) #Here I made a print statement that prints "Rotated square" and the number that the rotated square variable is holding which will be the tupples in the square variables list.
+result_3d = get_3d_magnitude(2, 3, 6) #Here I made a result_3d variable and inside it I have it call the get_3d_magnitude function with 2 as dx, 3 as dy and 6 as dz.
+print("3D Magnitude:", round(result_3d, 2)) #Here I made a print statement that prints "3D Magnitude:" and the number that the result_3d variable is holding rounded two decimal places.
